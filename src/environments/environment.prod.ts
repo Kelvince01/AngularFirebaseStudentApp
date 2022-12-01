b47034b1c3bdcff33215a65082c27f1d50add9ec
Kelvince01 <themikrochip1@gmail.com>
2022-12-01 16:26:53 +0300

    firebase config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    databaseURL: "https://angularfirebasestudentap-90d4c-default-rtdb.firebaseio.com/",
+    apiKey: "AIzaSyCtm3ybocFBV1ouPKVbyyn5QgqYyDPdaeA",
+    authDomain: "angularfirebasestudentap-90d4c.firebaseapp.com",
+    projectId: "angularfirebasestudentap-90d4c",
+    storageBucket: "angularfirebasestudentap-90d4c.appspot.com",
+    messagingSenderId: "174092457735",
+    appId: "1:174092457735:web:644f0695a52e098d1757a2",
+    measurementId: "G-062WPVCMR3"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,13 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-    apiKey: "xxxxxx-xxxxxx_xxxxxxxxxxxxxxxxxx",
-    authDomain: "xxxxxx_xxxxxxxxxxxxxxxxxx",
-    databaseURL: "xxxxxx_xxxxxxxxxxxxxxxxxx",
-    projectId: "xxxxxx_xxxxxxxxxxxxxxxxxx",
-    storageBucket: "xxxxxx_xxxxxxxxxxxxxxxxxx",
-    messagingSenderId: "xxxxxxxxxxxxxxxxxx",
-    appId: "1:xxxxxxxxxxxxxxxxxx:web:xxxxxxxxxxxxxxxxxx"
+    databaseURL: "https://angularfirebasestudentap-90d4c-default-rtdb.firebaseio.com/",
+    apiKey: "AIzaSyCtm3ybocFBV1ouPKVbyyn5QgqYyDPdaeA",
+    authDomain: "angularfirebasestudentap-90d4c.firebaseapp.com",
+    projectId: "angularfirebasestudentap-90d4c",
+    storageBucket: "angularfirebasestudentap-90d4c.appspot.com",
+    messagingSenderId: "174092457735",
+    appId: "1:174092457735:web:644f0695a52e098d1757a2",
+    measurementId: "G-062WPVCMR3"
   }
 };
